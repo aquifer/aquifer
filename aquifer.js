b758e4d94185f89494c8ad3681d52ea1e1c7d304
Patrick Coffey <patrickcoffey48@gmail.com>
2015-05-08 14:19:05 -0500

    Add basic cli tools

---
+++ aquifer.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+/**
+ * @file
+ * Defines aquifer command line interface.
+ *
+ *                    _  __
+ *   __ _  __ _ _   _(_)/ _| ___ _ __
+ *  / _` |/ _` | | | | | |_ / _ \ '__|
+ * | (_| | (_| | |_| | |  _|  __/ |
+ *  \__,_|\__, |\__,_|_|_|  \___|_|
+ *           |_|
+ */
+/* globals require */
+
+var Aquifer = {};
+Aquifer.console = require('./lib/console.api.js')(Aquifer);
+Aquifer.init = require('./lib/init.api.js')(Aquifer);
+Aquifer.init.setup();
+
+// If no arguments passed in, output cli docs. Else parse.
+if (!process.argv.slice(2).length) {
+  Aquifer.console.log(Aquifer.art + '\n', 'success');
+  if (Aquifer.initialized === false) {
+    Aquifer.console.log('To create a Drupal site, run: "aquifer create <sitename>"', 'notice');
+  }
+
+  Aquifer.cli.outputHelp();
+}
+else {
+  Aquifer.cli.parse(process.argv);
+}
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Gulp tasks for this project.
+ */
+/* globals require */
+
+'use strict';
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    jsFilePatterns = [
+      'index.js',
+      'lib/*.js',
+      'lib/**/*.js'
+    ];
+
+/**
+ * @task lint
+ *   Runs JSCS and JSLint on module, theme, and gulp files. Excludes all
+ *   minified JavaScript files.
+ */
+gulp.task('lint', function () {
+  return gulp.src(jsFilePatterns)
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(jscs());
+});
+
+/**
+ * @task watch
+ * Runs lint tasks when files are changed.
+ */
+gulp.task('watch', function() {
+  gulp.watch(jsFilePatterns, ['lint']);
+});
