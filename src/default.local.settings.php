72f4f85022ea7dca4d9b5b1204d7835fa529b44f
Jeff Tomlinson <jeff@fourkitchens.com>
2015-09-04 02:37:47 -0600

    Removing obsolete documentation.

--- src/default.local.settings.php
+++ src/default.local.settings.php
@@ -2,11 +2,6 @@
 /**
  * @file
  * Local settings for this environment.
- *
- * If this file is named local.settings.php it will be kept out of version
- * control and can contain local configuration for development purposes.
- *
- * @see https://www.drupal.org/node/1525472
  */
 
 $conf['preprocess_css'] = 0;
--- src/default.secret.settings.php
+++ src/default.secret.settings.php
@@ -2,7 +2,4 @@
 /**
  * @file
  * Secret settings for the site.
- *
- * If this file is named secret.settings.php it will be kept out of version
- * control to protect sensitive site information.
  */
